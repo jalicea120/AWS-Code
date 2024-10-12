189cfefc0cec71cf3da6cf78612d1a8347398469
Juan <aliceajuan41@gmail.com>
2024-10-11 22:10:08 -0400

    First One

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "my-first-ec2" {
+resource "aws_instance" "my-first-ec2-help" {
     ami = "ami-0b5eea76982371e91"
     instance_type = "t2.micro"
 }
